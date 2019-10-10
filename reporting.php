e102769e2ceb995d54cc09503e88f1f6021f6d4d
Kudzaishe Ranganai <u17032220@tuks.co.za>
2019-10-10 17:17:42 +0200

    leave

--- reporting.php
+++ reporting.php
@@ -348,7 +348,7 @@
             </div>
         </div>
       </div>
-            <div class="modal fade" id="generateDateSale" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal fade" id="generateDateSale" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
           <div class="modal-content">
             <div class="modal-header">
@@ -367,7 +367,7 @@
                     </select>
                   </div>
                 </div>
-                <input type="submit" class="btn btn-success" value="Generate Report"></input>
+                <input type="submit" class="btn btn-success" value="Generate Report">
                 <button type="button" class="btn btn-danger" data-dismiss="modal">No</button>
               </form>
           </div>
@@ -399,45 +399,35 @@
 
 
       <div class="col-md-2 errorModal successModal text-center">
-                            <div class="modal fade"  tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true" id="displayModal">
-                              <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
-                                  <div class="modal-content">
-                                    
-                                      <div class="modal-header" id="modalHeader">
+          <div class="modal fade"  tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true" id="displayModal">
+            <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
+                <div class="modal-content">
+                  
+                    <div class="modal-header" id="modalHeader">
 
-                                          <h6 class="modal-title" id="modal-title-default2" ></h6>
-                                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                              <span aria-hidden="true">×</span>
-                                          </button>
-                                      </div>
-                                      
-                                      <div class="modal-body">
-                                          <p id="MMessage"></p>
-                                            <div id="animation" style="text-align:center;">
+                        <h6 class="modal-title" id="modal-title-default2" ></h6>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    
+                    <div class="modal-body">
+                        <p id="MMessage"></p>
+                          <div id="animation" style="text-align:center;">
 
-                                            </div>
-                                          
-                                      </div>
-                                      
-                                      <div class="modal-footer">
-                                          
-                                          <button type="button" class="btn btn-link  ml-auto" data-dismiss="modal" id="btnClose">Close</button>
-                                      </div>
-                                      
-                                  </div>
-                              </div>
-                            </div>
                           </div>
-                        </div>
-
-
-
-
-
-
-
-
-
+                        
+                    </div>
+                    
+                    <div class="modal-footer">
+                        
+                        <button type="button" class="btn btn-link  ml-auto" data-dismiss="modal" id="btnClose">Close</button>
+                    </div>
+                    
+                </div>
+            </div>
+          </div>
+        </div>
       <div class="modal fade" id="generateDate" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
           <div class="modal-content">
@@ -482,7 +472,6 @@
             </div>
         </div>
       </div>
-    </div>
       <?php include_once("footer.php");?>
     </div>
   </div>
